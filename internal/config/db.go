222467cb0d51a89f09f28df315bdd1de01574f20
Sávio Alexandre <savioalefreitas@gmail.com>
2024-12-08 23:47:54 -0300

    add dot env to get configs

--- internal/config/db.go
+++ internal/config/db.go
@@ -24,7 +24,7 @@ func ConnectDB() (*sql.DB, error) {
 	password := os.Getenv("DB_PASSWORD")
 	dbname := os.Getenv("DB_NAME")
 
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s  sslmode=disable", host, port, user, password, dbname)
+	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s  sslmode=disable", host, port, user, password, dbname)
 
 	db, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
